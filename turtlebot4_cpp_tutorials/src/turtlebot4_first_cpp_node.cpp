61d676ef5d142f3e234bc11d1f2602a213cf9b64
Quang Doan <quang.m.doan@gmail.com>
2026-03-22 04:35:25 -0700

    wire up deps for urtlebot4_first_cpp_node

--- turtlebot4_cpp_tutorials/src/turtlebot4_first_cpp_node.cpp
+++ turtlebot4_cpp_tutorials/src/turtlebot4_first_cpp_node.cpp
@@ -1,4 +1,12 @@
-#include <cstdio>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "irobot_create_msgs/msg/interface_buttons.hpp"
+#include "irobot_create_msgs/msg/lightring_leds.hpp"
 
 int main(int argc, char ** argv)
 {
