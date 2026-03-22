ae11b7e0ec8d7a5d8695b057e9c6b7377c19bfe2
Quang Doan <quang.m.doan@gmail.com>
2026-03-22 05:50:36 -0700

    TurtleBot4 Standard has HMI buttons published on /hmi/buttons

--- turtlebot4_cpp_tutorials/src/turtlebot4_first_cpp_node.cpp
+++ turtlebot4_cpp_tutorials/src/turtlebot4_first_cpp_node.cpp
@@ -1,18 +1,41 @@
-#include <chrono>
-#include <functional>
 #include <memory>
-#include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "turtlebot4_msgs/msg/user_button.hpp"
 
-#include "irobot_create_msgs/msg/interface_buttons.hpp"
-#include "irobot_create_msgs/msg/lightring_leds.hpp"
-
-int main(int argc, char ** argv)
+class TurtleBot4FirstNode : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+  TurtleBot4FirstNode() : Node("turtlebot4_first_cpp_node")
+  {
+    // Subscribe to the /hmi/buttons topic (TurtleBot4 Standard HMI buttons)
+    hmi_buttons_subscriber_ = this->create_subscription<turtlebot4_msgs::msg::UserButton>(
+      "/hmi/buttons",
+      rclcpp::SensorDataQoS(),
+      [this](const turtlebot4_msgs::msg::UserButton::SharedPtr msg) {
+        hmi_buttons_callback(msg);
+      });
+  }
 
-  printf("hello world turtlebot4_cpp_tutorials package\n");
+private:
+  // HMI buttons subscription callback
+  void hmi_buttons_callback(
+    const turtlebot4_msgs::msg::UserButton::SharedPtr buttons_msg)
+  {
+    // Button 1 is pressed (index 0)
+    if (buttons_msg->button[0]) {
+      RCLCPP_INFO(this->get_logger(), "Button 1 Pressed!");
+    }
+  }
+
+  // HMI Button Subscriber
+  rclcpp::Subscription<turtlebot4_msgs::msg::UserButton>::SharedPtr hmi_buttons_subscriber_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<TurtleBot4FirstNode>());
+  rclcpp::shutdown();
   return 0;
 }
